b48f83a03576a0ab16b95f4c316990e57c5fa6ec
ashishmusani <ashish_musani@ymail.com>
2020-10-11 12:23:16 +0530

    disabled calling to and from mobile devices

--- src/components/ChatScreen/index.js
+++ src/components/ChatScreen/index.js
@@ -29,8 +29,6 @@ const myPeer = new Peer(undefined,{
 })
 var peer_call;
 var local_stream;
-var callRingingInterval;
-
 
 const initialCallState = {
   isInCall: false,
@@ -72,6 +70,10 @@ function callStateReducer(state, action){
   }
 }
 
+const x = navigator.userAgent||navigator.vendor||window.opera;
+const isMobileDevice = (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(x)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(x.substr(0,4)))
+let callTimeout;
+
 export default function ChatScreen(props){
 
   const Context = useContext(LoginContext);
@@ -87,6 +89,7 @@ export default function ChatScreen(props){
 
 
   const [onlineUsers, setOnlineUsers] = useState([]);
+  const [onlineUsersInfo, setOnlineUsersInfo] = useState([]);
   const [conversation, setConversation] = useState({});
   const [unreadMessagesCount, setUnreadMessages] = useState({});
   const [newMessage, setNewMessage] = useState("");
@@ -99,7 +102,7 @@ export default function ChatScreen(props){
 
   useEffect(()=>{
     socket.open();
-    socket.emit('login', username);
+    socket.emit('login', username, isMobileDevice);
     const hist = JSON.parse(sessionStorage.getItem('conversation'));
     if(hist){
       setConversation(hist);
@@ -131,7 +134,10 @@ export default function ChatScreen(props){
       updateConversation('incoming', envelope);
     })
     socket.on('online-users-list', (userList)=>{
-      setOnlineUsers(userList.filter(uname => (uname !== username)));
+      setOnlineUsersInfo(userList);
+      let onlineUsernames = userList.map(({username: uname}) => (uname))
+                                    .filter(uname => (uname !== username));
+      setOnlineUsers(onlineUsernames);
     })
     socket.on('personal_message', (envelope)=>{
       MessageNotificationSound.play();
@@ -140,8 +146,6 @@ export default function ChatScreen(props){
     })
     socket.on('disconnect', (reason) =>{
       if(!(reason === 'io client disconnect')){
-        handleLogout();
-        alert("Disconnected from server");
         socket.connect();
       }
     })
@@ -163,10 +167,9 @@ export default function ChatScreen(props){
 
   useEffect(()=>{
     if(callState.callType === 'outgoing' && callState.callStatus === 'ringing'){
-      setTimeout(()=>{
-        console.log("Outgoing call timeout");
+      callTimeout = setTimeout(()=>{
         endCall(true)
-      },5000)
+      },30000)
     }
   }, [callState])
 
@@ -272,6 +275,9 @@ export default function ChatScreen(props){
     if(sendingTo === 'conference'){
       alert('Oops! Conference calls are not yet supported!')
     }
+    else if(onlineUsersInfo.find(user => (user.username === sendingTo)).isMobileDevice){
+      alert(`Oops! Cannot call ${sendingTo} as the user is logged from mobile device !`)
+    }
     else{
       console.log('Placing audio call request');
       OutgoingCallRingSound.play();
@@ -324,8 +330,9 @@ export default function ChatScreen(props){
   }
 
   function endCall(fromSelf){
+    clearTimeout(callTimeout)
     resetNotificationSounds();
-    CallDropNotificationSound.play();
+    if(callState.callType === 'outgoing') CallDropNotificationSound.play();
     console.log("ending "+ callState.callType  + " call");
     myPeer.removeAllListeners();
     if(fromSelf){
@@ -392,9 +399,11 @@ export default function ChatScreen(props){
                             placeholder="Type a message" value={newMessage}
                             onChange={(e)=> setNewMessage(e.target.value)} autocomplete="off" autoFocus/>
                         <InputGroup.Append>
-                          <Button variant="secondary" onClick={()=> initiate_audio_call()}>
-                            <Telephone />
-                          </Button>
+                          {isMobileDevice? null: (
+                            <Button variant="secondary" onClick={()=> initiate_audio_call()}>
+                              <Telephone />
+                            </Button>
+                          )}
                           <Button variant="secondary" onClick={()=> {sendFileDialogRef.current.click()}}>
                             <Camera />
                           </Button>
