24343914b2b5ea18df99dc82e88f2b4d1f002942
ashishmusani <ashish_musani@ymail.com>
2020-10-14 22:25:30 +0530

    Client now gets list of all users + information about each users online status and last seen time

--- src/components/ChatScreen/OnlineUsersList/UserView/index.js
+++ src/components/ChatScreen/OnlineUsersList/UserView/index.js
@@ -3,26 +3,38 @@ import React from 'react';
 import './UserView.css'
 
 export default function UserView(props){
-
   const user =  props.user;
   const setSendingTo = props.setSendingTo;
   const sendingTo = props.sendingTo;
   const unreads = props.unreads;
   const closeOnlineUsersModal =  props.closeOnlineUsersModal;
 
+  let lastSeenAt;
+  if(user.lastSeen && !user.isOnline){
+    let lastSeenDate = new Date(user.lastSeen)
+    let currentDate = new Date()
+    let timeDiff = currentDate - lastSeenDate;
+    lastSeenAt = `${lastSeenDate.getDate()}/${lastSeenDate.getMonth()+1} ${lastSeenDate.getHours()}:${lastSeenDate.getMinutes()}`;
+  }
+
   const changeConversationWindow = () =>{
-    setSendingTo(user);
+    setSendingTo(user.username);
     closeOnlineUsersModal();
   }
 
-  //console.log(user + ": ", unreads)
-
   return (
-    <div className={sendingTo === user? "online-user-selected" : "online-user"} onClick={changeConversationWindow}>
+    <div className={sendingTo === user.username? "online-user-selected" : "online-user"} 
+        onClick={changeConversationWindow}>
       <div className="online-user__icon">
       </div>
       <div className="online-user__details">
-      <strong>{user} </strong>
+      <strong>{user.username}</strong><br/>
+      {user.username !== "conference"? 
+            ((user.isOnline)? 
+                "Online" : 
+                (user.lastSeen? `Last seen ${lastSeenAt}` : "Offline")
+            ) : 
+            null}
       </div>
       {unreads>0? (<div className="online-user__details__unreads">{unreads}</div>) : null}
     </div>
--- src/components/ChatScreen/OnlineUsersList/index.js
+++ src/components/ChatScreen/OnlineUsersList/index.js
@@ -6,13 +6,13 @@ export default function OnlineUsersList(props){
   const sendingTo = props.sendingTo;
   const setSendingTo = props.setSendingTo;
   var onlineUsers =  props.onlineUsers;
-  onlineUsers = ["conference", ...onlineUsers];
+  onlineUsers = [{username: "conference"}, ...onlineUsers];
   var unreadMessagesCount =  props.unreadMessagesCount;
   const closeOnlineUsersModal = props.closeOnlineUsersModal;
 
   return (
     <div className="online-users-list">
-      {onlineUsers.map(user => (<UserView user={user} unreads={unreadMessagesCount[user]} sendingTo={sendingTo} setSendingTo={setSendingTo} closeOnlineUsersModal={closeOnlineUsersModal}/>))}
+      {onlineUsers.map(user => (<UserView user={user} unreads={unreadMessagesCount[user.username]} sendingTo={sendingTo} setSendingTo={setSendingTo} closeOnlineUsersModal={closeOnlineUsersModal}/>))}
     </div>
   )
 }
