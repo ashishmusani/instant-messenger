137220ceeb36c2fb26d66f39575b10adf957f8d4
ashishmusani <ashish_musani@ymail.com>
2020-10-17 15:31:29 +0530

    more changes to prepare for release

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
