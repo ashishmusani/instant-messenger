08b2db0b7b182f6ee8d4f39facb0e2f2308994f1
ashishmusani <ashish_musani@ymail.com>
2020-10-17 15:54:27 +0530

    correction to hiding db uri

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, './client/build')));
 
-mongoose.connect(process.env.DB_URI, {'useNewUrlParser': true, useUnifiedTopology: true } );
+mongoose.connect(`${process.env.DB_URI}`, {'useNewUrlParser': true, useUnifiedTopology: true } );
 const User = require('./models/userModel');
 
 io.on('connect',socket=>{
