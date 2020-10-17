bd532852e6096eed4aad53dd17a0e0399e353f9f
ashishmusani <ashish_musani@ymail.com>
2020-10-17 15:20:39 +0530

    changes to prepare for release

---
+++ client/build/precache-manifest.16356fa9c48aa4f4656a018efc77bc5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd64a180a9c51320257a25ebfdddf906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58bf650428288bd3fc37",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a9454e72a98a2af0ea9e",
+    "url": "/static/css/main.0476040c.chunk.css"
+  },
+  {
+    "revision": "58bf650428288bd3fc37",
+    "url": "/static/js/2.11e6ac87.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.11e6ac87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9454e72a98a2af0ea9e",
+    "url": "/static/js/main.cb9d783f.chunk.js"
+  },
+  {
+    "revision": "34486f9a51f2abc23aa0",
+    "url": "/static/js/runtime-main.2ccd3a3d.js"
+  }
+]);
